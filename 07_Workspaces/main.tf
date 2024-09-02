0c1b020a0052407193fc3151ce58383b7adfe061
Meletis Papadopoulos <meletiop22@hotmail.com>
2024-09-02 13:14:32 +0300

    Update main.tf file

--- 07_Workspaces/main.tf
+++ 07_Workspaces/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_names[terraform.workspace]
+  name     = var.resource_group_names[terraform.workspace] # Mapping
   location = "westeurope"
 }
